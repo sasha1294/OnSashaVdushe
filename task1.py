bc74c17e3550ded379b5ec6be19efba661440078
rom4na4 <rom4na4@yandex.ru>
2023-02-07 08:21:49 +0900

    Задания для студентов!

---
+++ task1.py
@@ -0,0 +1,29 @@
+#Возвращает функцию, которая обрезает строку (задан первый аргумент), если она длиннее максимальной длины строки
+# (задан второй аргумент). Результат также должен заканчиваться на "..."
+#Эти точки в конце также увеличивают длину строки.
+#Итак, в приведенном выше примере trim("Создание ката - это весело", 14) должен возвращать "Создание ка..."
+#Если максимальная длина строки меньше или равна 3 символам, то длина точек не добавляется к длине строки.
+#например, trim("Он", 1) должен возвращать "H...", потому что 1 <= 3
+#Если строка меньше или равна максимальной длине строки, то просто верните строку без необходимости обрезки или точек.
+#например, trim("Code Wars is pretty rad", 50) должен возвращать "Code Wars is pretty rad".
+
+#task1 Test example
+# import codewars_test as test
+# from solution import trim
+#
+# @test.describe("trim")
+# def tests():
+#
+#     @test.it("works for some examples")
+#     def _():
+#         test.assert_equals(trim("Creating kata is fun", 14),"Creating ka...")
+#         test.assert_equals(trim("He", 1),"H...")
+#         test.assert_equals(trim("Hey", 2),"He...")
+#         test.assert_equals(trim("Hey", 3),"Hey")
+#         test.assert_equals(trim("Coding rocks", 12),"Coding rocks")
+#         test.assert_equals(trim("Code Wars is pretty rad", 50), "Code Wars is pretty rad")
+#         test.assert_equals(trim("London is freezing",18),"London is freezing")
+
+# Start resolving! Good luck!
+def trim(phrase, size):
+    raise NotImplementedError("TODO: trim")
---
+++ task2.py
@@ -0,0 +1,24 @@
+# Ваша задача в этом ката - реализовать функцию, которая вычисляет сумму целых чисел внутри строки.
+# Например, в строке "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog" сумма целых чисел равна 3635.
+# Примечание: будут проверяться только положительные целые числа.
+
+# Task2 Test example
+# test.describe("Example test cases")
+#
+# exampleTests = (
+# ("12.4", 16),
+# ("h3ll0w0rld", 3),
+# ("2 + 3 = ", 5),
+# ("Our company made approximately 1 million in gross revenue last quarter.", 1),
+# ("The Great Depression lasted from 1929 to 1939.", 3868),
+# ("Dogs are our best friends.", 0),
+# ("C4t5 are 4m4z1ng.", 18),
+# ("The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog", 3635)
+# )
+#
+# for testString, correctAnswer in exampleTests:
+#     test.assert_equals(sum_of_integers_in_string(testString), correctAnswer)
+
+# Start resolving! Good luck!
+def sum_of_integers_in_string(s):
+    return#what the function name says
